100fdba74a9aee5be8230ac721ca8b99bf7eb96f
fomhair <box.offlame@gmail.com>
2022-10-11 20:16:38 +0300

    Hello Oleg program

---
+++ example005/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "олег") {
+  Console.WriteLine("Привет, "+ username+ ", дружище!");
+} else {
+  Console.WriteLine("Привет, "+ username);
+}
